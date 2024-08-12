cc6575dc96d5ac1abe81fd0c7119255a02ee7e93
SABEYGUNA <SABEYGUNA@csr.com.au>
2024-08-12 22:05:37 +0530

    auth.js setup with few providers

--- src/configuration/auth.ts
+++ src/configuration/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
+import Github from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import Facebook from "next-auth/providers/facebook"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Github, Google, Facebook],
 })
